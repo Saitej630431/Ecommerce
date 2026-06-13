7e8bf36a6c2cdc9edfc13d49fe5c807394c065cc
Saitej630431 <saitejapakala2001@gmail.com>
2026-06-13 15:18:28 +0530

    secondchange

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,21 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { HashRouter } from 'react-router-dom';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
+    
   </StrictMode>
 )
  
